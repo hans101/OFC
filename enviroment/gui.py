56ac6ad2c6b49719649fe7aefb4b56e22f4e6490
damianlapa <damian.maksymilian.lapa@gmail.com>
2020-05-12 08:16:49 +0200

    displaying burned cards

--- enviroment/gui.py
+++ enviroment/gui.py
@@ -25,9 +25,6 @@ first_player_label.place(x=375, y=0)
 board = Canvas(first_player_frame, bg='black', width=700, height=420)
 board.place(x=50, y=25)
 
-# first round/dealing 5 cards
-all_hand_cards = [deck.get_card() for card in range(1, 18)]
-
 # creating grid
 board.create_line(0, 105, 700, 105, width=3, fill='grey', tag='linia')
 board.create_line(0, 210, 700, 210, width=3, fill='grey')
@@ -40,60 +37,98 @@ board.create_line(375, 0, 375, 420, width=3, fill='grey')
 board.create_line(445, 0, 445, 420, width=3, fill='grey')
 board.create_line(515, 105, 515, 420, width=3, fill='grey')
 
-# placing cards on board
-board.create_image(200, 370, image=all_hand_cards[0].representation(), tags=('draw',))
-board.create_image(270, 370, image=all_hand_cards[1].representation(), tags=('draw',))
-board.create_image(340, 370, image=all_hand_cards[2].representation(), tags=('draw',))
-board.create_image(410, 370, image=all_hand_cards[3].representation(), tags=('draw',))
-board.create_image(480, 370, image=all_hand_cards[4].representation(), tags=('draw',))
+# creating back card image
+back_card_image = ImageTk.PhotoImage(Image.open("Cards/green_back.png"))
+board.create_image(130, 370, image=back_card_image, tag='card_back', state=HIDDEN)
+all_hand_cards = [deck.get_card() for card in range(1, 18)]
+all_hand_cards.insert(0, back_card_image)
+
+switch = 0
+
+
+def create_new_card_image(card, x_position, y_position):
+    board.create_image(x_position, y_position, image=card.representation(), tags=('draw', 'bottom_row'))
+
+
+# first draw(5 cards)
+def first_draw():
+    [create_new_card_image(all_hand_cards[i], (130 + (i * 70)), 370) for i in range(1, 6)]
 
 
 def returning_card_of_image_object(image_object):
     image_name = board.itemcget(image_object, 'image')
     result = re.search(r'\d+', image_name)
-    card = all_hand_cards[int(result.group())-1]
+    card = all_hand_cards[int(result.group()) - 1]
     return card
 
-for cardd in board.find_withtag('draw'):
-    print(returning_card_of_image_object(cardd))
+
+first_draw()
+
+
+def row_reload(row, x_position, y_position):
+    for i in range(0, len(row)):
+        board.coords(row[i], (x_position + (i * 70)), y_position)
+
+
+def all_rows_reload(bottom_row, row1, row2, row3):
+    if len(bottom_row) > 4:
+        row_reload(bottom_row, 200, 370)
+    else:
+        row_reload(bottom_row, 270, 370)
+    row_reload(row1, 200, 265)
+    row_reload(row2, 200, 160)
+    row_reload(row3, 270, 55)
+
 
 # creating set button
 def set_position():
     row1 = board.find_withtag('row1')
     row2 = board.find_withtag('row2')
     row3 = board.find_withtag('row3')
+    bottom_row = board.find_withtag('bottom_row')
+
+    if bottom_row:
+        board.addtag_withtag('burn', 'bottom_row')
+        burned_cards = board.find_withtag('burn')
+        for i in range(0, len(burned_cards)):
+            board.itemconfig(burned_cards[i], tag='burn')
+            board.itemconfig(burned_cards[i], state=HIDDEN)
+            board.coords(burned_cards[i], (130 - i*30), 370)
 
     card_number = len(row1) + len(row2) + len(row3)
+    possible_cards_quantity = [5, 7, 9, 11, 13]
 
-    # placing cards on board
-    if card_number == 5:
-        board.dtag('draw')
-        board.create_image(200, 365, image=all_hand_cards[5].representation(), tags=('draw',))
-        board.create_image(270, 365, image=all_hand_cards[6].representation(), tags=('draw',))
-        board.create_image(340, 365, image=all_hand_cards[7].representation(), tags=('draw',))
-    elif card_number == 7:
-        board.dtag('draw')
-        board.create_image(200, 365, image=all_hand_cards[8].representation(), tags=('draw',))
-        board.create_image(270, 365, image=all_hand_cards[9].representation(), tags=('draw',))
-        board.create_image(340, 365, image=all_hand_cards[10].representation(), tags=('draw',))
-    elif card_number == 9:
-        board.dtag('draw')
-        board.create_image(200, 365, image=all_hand_cards[11].representation(), tags=('draw',))
-        board.create_image(270, 365, image=all_hand_cards[12].representation(), tags=('draw',))
-        board.create_image(340, 365, image=all_hand_cards[13].representation(), tags=('draw',))
-    elif card_number == 11:
-        board.dtag('draw')
-        board.create_image(200, 365, image=all_hand_cards[14].representation(), tags=('draw',))
-        board.create_image(270, 365, image=all_hand_cards[15].representation(), tags=('draw',))
-        board.create_image(340, 365, image=all_hand_cards[16].representation(), tags=('draw',))
-    elif card_number == 13:
-        board.dtag('draw')
+    card_back = board.find_withtag('card_back')
 
-        top = [returning_card_of_image_object(card) for card in row3]
-        mid = [returning_card_of_image_object(card) for card in row2]
-        bottom = [returning_card_of_image_object(card) for card in row1]
-        new_hand = OfcHand(top=top, bot=bottom, mid=mid)
-        print(new_hand)
+    if card_number in possible_cards_quantity:
+        # placing cards on board
+        if card_number == 5:
+            board.dtag('draw')
+            # second draw(3 cards)
+            [create_new_card_image(all_hand_cards[i], (-150 + (i * 70)), 370) for i in range(6, 9)]
+        elif card_number == 7:
+            board.dtag('draw')
+            board.itemconfig(card_back, state=NORMAL)
+            # third draw(3 cards)
+            [create_new_card_image(all_hand_cards[i], (-360 + (i * 70)), 370) for i in range(9, 12)]
+        elif card_number == 9:
+            board.dtag('draw')
+            # fourth draw(3 cards)
+            [create_new_card_image(all_hand_cards[i], (-570 + (i * 70)), 370) for i in range(12, 15)]
+        elif card_number == 11:
+            board.dtag('draw')
+            # fifth_draw(5 cards)
+            [create_new_card_image(all_hand_cards[i], (-780 + (i * 70)), 370) for i in range(15, 18)]
+        elif card_number == 13:
+            board.dtag('draw')
+
+            top = [returning_card_of_image_object(card) for card in row3]
+            mid = [returning_card_of_image_object(card) for card in row2]
+            bottom = [returning_card_of_image_object(card) for card in row1]
+            new_hand = OfcHand(top=top, bot=bottom, mid=mid)
+            print(new_hand)
+    else:
+        pass
 
 
 button = Button(board, text='SET', command=set_position)
@@ -123,24 +158,54 @@ def tag_add(event):
         board.dtag(selected_card, 'row1')
         board.dtag(selected_card, 'row2')
         board.dtag(selected_card, 'row3')
+        board.dtag(selected_card, 'bottom_row')
         board.addtag_enclosed('row1', 165, 165, 515, 350)
         board.addtag_enclosed('row2', 165, 75, 515, 250)
         board.addtag_enclosed('row3', 165, 0, 515, 150)
+        board.addtag_enclosed('bottom_row', 165, 285, 515, 500)
         row1 = board.find_withtag('row1')
         row2 = board.find_withtag('row2')
         row3 = board.find_withtag('row3')
-        drew_row = board.find_withtag('drew')
+        bottom_row = board.find_withtag('bottom_row')
         if selected_card in row1:
             board.coords(selected_card, (130 + len(row1) * 70), 265)
+            row_reload(row1, 200, 265)
         elif selected_card in row2:
             board.coords(selected_card, (130 + len(row2) * 70), 160)
+            row_reload(row2, 200, 160)
         elif selected_card in row3:
             board.coords(selected_card, (200 + len(row3) * 70), 55)
+            row_reload(row3, 270, 55)
         else:
-            board.coords(selected_card, (130 + len(drew_row) * 70), 370)
+            board.itemconfig(selected_card, tags=('draw', 'bottom_row'))
+            board.coords(selected_card, (130 + len(bottom_row) * 70), 370)
+            row_reload(bottom_row, 200, 370)
+
+        row1 = board.find_withtag('row1')
+        row2 = board.find_withtag('row2')
+        row3 = board.find_withtag('row3')
+        bottom_row = board.find_withtag('bottom_row')
+
+        all_rows_reload(bottom_row, row1, row2, row3)
+
+
+def display_burned_cards(event):
+    global switch
+    switch += 1
+    # show/hide burned cards:
+    burned_cards = board.find_withtag('burn')
+    if switch %2 == 0:
+        if burned_cards:
+            for card in burned_cards:
+                board.itemconfig(card, state=NORMAL)
+    else:
+        if burned_cards:
+            for card in burned_cards:
+                board.itemconfig(card, state=HIDDEN)
 
 
 board.bind("<B1-Motion>", card_move)
 board.bind('<ButtonRelease-1>', tag_add)
+board.bind('<3>', display_burned_cards)
 
 root.mainloop()
